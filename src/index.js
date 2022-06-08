390a95e530adaac561bda990e2b3c3a155a140dd
atkeppu <turpeinen.sami@gmail.com>
2022-06-08 21:18:54 +0300

    WEEK 1
    WEEK 1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+document.getElementById("add-comment").addEventListener('click', AddUnsortList)
+document.getElementById("remove-comments").addEventListener('click', RemoveAllComments)
+
+function AddUnsortList() {
+        var ul = document.getElementById("UnsortList");
+        var textare = document.getElementById("textare");                    
+        ul.innerHTML += '<li>' +textare.value +' </li>';        
+      };
+
+      function RemoveAllComments() 
+      {  
+        if (confirm("Do you really wants to delete all the comments?") == true) 
+        {
+          var ul = document.getElementById("UnsortList");
+          ul.innerHTML = '';
+        } 
+        else {}
+      };
+
+
+
+
